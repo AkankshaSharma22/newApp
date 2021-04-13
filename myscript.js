54e950ed5b3695379c44979cd44279bbb1b93e03
Vimlesh Kumar <vimlesh@kgisl.com>
2021-04-13 10:28:19 +0530

    script is added

---
+++ myscript.js
@@ -0,0 +1,8 @@
+function greeting(){
+
+    console.log('Greetings from servicenow sample app');
+    alert('Greetings from servicenow sample app');
+
+    document.getElementById('out').innerHTML = 'Greetings from servicenow sample app';
+    
+}
